69bfca90d3658298c621cb2ed2e6f1c16ee6093e
ojomaze <jbobjerg@gmail.com>
2026-04-08 12:07:21 +0200

    Added attributes to Employee Added Role class Added Qualification class

--- SportCenter.Api/Models/Employee.cs
+++ SportCenter.Api/Models/Employee.cs
@@ -1,6 +1,23 @@
 namespace SportCenter.Api.Models;
 public class Employee {
 
-    //test
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Phone { get; set; }
+    
+    public string Email { get; set; }
+
+    public int Id { get; set; }
+
+    private static int _id = 0;
+
+    public Employee(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Id = _id++;
+    }
 
 }
---
+++ SportCenter.Api/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Role
+{
+	public string Name { get; set; }
+	public string Description { get; set; }
+
+	public Role(string name, string description)
+	{
+		Name = name;
+		Description = description;
+	}
+}
