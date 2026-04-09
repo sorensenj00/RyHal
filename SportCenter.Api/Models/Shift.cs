ca6c98dd5412a171b9e50cabe68d5db352945876
Mikkel99Bendsen <eaa24misb@students.eaaa.dk>
2026-04-09 08:21:27 +0200

    Shift-klasse oprettet uden links

--- SportCenter.Api/Models/Shift.cs
+++ SportCenter.Api/Models/Shift.cs
@@ -1,2 +1,31 @@
 namespace SportCenter.Api.Models;
-public class Shift {}
+public class Shift
+{
+    public Shift(DateOnly date, DateTime startTime, DateTime endTime, ShiftCategory category)
+    {
+        Date = date;
+        StartTime = startTime;
+        EndTime = endTime;
+        Category = category;
+    }
+
+    // link til employee her
+
+    public DateOnly Date {  get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    public ShiftCategory Category { get; set; }
+
+    enum ShiftCategory
+    {
+        HALL_BOY,
+        CLEANER,
+        ADMIN,
+        CAFE_WORKER,
+        DISHWASHER,
+        OTHER
+    }
+}
